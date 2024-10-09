c4da15d5ca533c3ad02b4dfe2e311d8f70e1c1f0
Christian Drumm <christian.drumm@hey.com>
2024-10-09 14:50:24 +0200

    fixed missing price in CSV

--- src/simple-erp/srv/simpleerp-api.js
+++ src/simple-erp/srv/simpleerp-api.js
@@ -1,22 +1,24 @@
-const cds = require('@sap/cds');
+const cds = require("@sap/cds");
 const { Products } = cds.entities;
 
 module.exports = (srv) => {
-  srv.on('getProducts', async req => {
+  srv.on("getProducts", async (req) => {
     const products = await SELECT.from(Products);
 
-    const csvHeader = 'productID,name,description,price,stock\n';
-    const csvRows = products.map(product => {
-      return [
-        product.productID,
-        product.name,
-        product.description,
-        product.price_value + ' ' + product.price_currency_code,
-        product.stock
-      ].join(',');
-    }).join('\n');
+    const csvHeader = "productID,name,description,price,stock\n";
+    const csvRows = products
+      .map((product) => {
+        return [
+          product.productID,
+          product.name,
+          product.description,
+          product.price + " " + product.currency_code,
+          product.stock,
+        ].join(",");
+      })
+      .join("\n");
 
-    req.res.set('Content-Type', 'text/csv');
+    req.res.set("Content-Type", "text/csv");
     return csvHeader + csvRows;
   });
 };
