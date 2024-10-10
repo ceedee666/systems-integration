20c71a364c5382c3bf0a5b148fa1c30c71d103c3
Christian Drumm <christian.drumm@gmail.com>
2024-10-10 10:35:48 +0000

    reduce stock when order is created

--- src/simple-erp/srv/simpleerp-api.cds
+++ src/simple-erp/srv/simpleerp-api.cds
@@ -4,6 +4,9 @@ service SimpleERPApi  @(
      impl    : 'srv/simpleerp-api'
 ) {
     function getProducts() returns String;
+    function products() returns String;
+    function stock(productID: String) returns Integer;
+    function createOrder() returns Boolean;
 }
 
 annotate SimpleERPApi with @(requires: 'system-integration');
--- src/simple-erp/srv/simpleerp-service.js
+++ src/simple-erp/srv/simpleerp-service.js
@@ -5,36 +5,31 @@ class SimpleERPService extends cds.ApplicationService {
     const { Customers, Orders, OrderItems, OrderStatus, Products } = this.entities;
 
     this.before("CREATE", Orders, async (req) => {
-      const { maxID } = await SELECT.one`max(orderID) as maxID`.from(Orders);
-      req.data.orderID = maxID + 1;
+      await this.calculateOrderID(req);
     });
 
     this.before("UPDATE", OrderItems.drafts, async (req) => {
+
       const itemDraft = await SELECT.one
         .from(OrderItems.drafts)
         .where({ ID: req.data.ID });
 
-      if (!itemDraft.itemID) {
-        const { maxID } = await SELECT.one("max(itemID) as maxID")
-          .from(req.query.target)
-          .where({ order_ID: itemDraft.order_ID });
-        req.data.itemID = maxID + 10;
-      }
-
-      let productID = req.data.product_ID
+      const productID = req.data.product_ID
         ? req.data.product_ID
         : itemDraft.product_ID;
-
-      let quantity = req.data.quantity ? req.data.quantity : itemDraft.quantity;
-      quantity = quantity ? quantity : 0;
 
       const product = await SELECT.one
         .from(Products)
         .where({ ID: productID });
 
-      req.data.itemAmount = product.price * quantity;
-      req.data.currency_code = product.currency_code;
+      let quantity = req.data.quantity ? req.data.quantity : itemDraft.quantity;
+      quantity = quantity ? quantity : 0;
+
+      await this.calculateItemID(req, itemDraft);
+      this.calculateItemAmount(req, product, quantity);
+      this.checkProductStock(req, product, quantity);
     });
+
 
     this.after("UPDATE", OrderItems.drafts, async (_, req) => {
       if (req.data.itemAmount) {
@@ -57,6 +52,37 @@ class SimpleERPService extends cds.ApplicationService {
       return res;
     });
 
+    this.on("CREATE", Orders, async (req, next) => {
+      const { ID, items } = req.data;
+      if (items){
+        for (let { product_ID, quantity } of items) {
+          let product = await SELECT.from (Products, product_ID, p => {p.productID, p.stock});
+          if(product.stock > quantity)
+            await UPDATE(Products, product_ID).with({stock: product.stock -= quantity});
+          else
+            req.error(409, `${quantity} exceeds stock for product #${product.productID}`, "quantity");
+        } 
+      }
+      return next()
+    });
+
+    this.on("pickOrder", (req) =>
+      UPDATE(req.subject).with({ orderStatus_status: 20 })
+    );
+    this.on("shipOrder", (req) =>
+      UPDATE(req.subject).with({ orderStatus_status: 30 })
+    );
+    this.on("completeOrder", (req) =>
+      UPDATE(req.subject).with({ orderStatus_status: 40 })
+    );
+    this.on("cancelOrder", (req) =>
+      UPDATE(req.subject).with({ orderStatus_status: -10 })
+    );
+
+    this.after("each", Orders, (res, req) => calculateButtonAvailability(res, req));
+    this.after("each", Orders.drafts, (res, req) => calculateButtonAvailability(res, req, true));
+
+
     this.updateOrderAmount = async function (order_ID, target) {
       const itemDrafts = await SELECT.from(target).where({
         order_ID,
@@ -77,18 +103,6 @@ class SimpleERPService extends cds.ApplicationService {
       });
     };
 
-    this.on("pickOrder", (req) =>
-      UPDATE(req.subject).with({ orderStatus_status: 20 })
-    );
-    this.on("shipOrder", (req) =>
-      UPDATE(req.subject).with({ orderStatus_status: 30 })
-    );
-    this.on("completeOrder", (req) =>
-      UPDATE(req.subject).with({ orderStatus_status: 40 })
-    );
-    this.on("cancelOrder", (req) =>
-      UPDATE(req.subject).with({ orderStatus_status: -10 })
-    );
 
     const calculateButtonAvailability = (res, req, draft = false) => {
       const status = res.orderStatus_status;
@@ -98,9 +112,31 @@ class SimpleERPService extends cds.ApplicationService {
       res.cancelEnabled = draft ? false : status === 10;
     };
 
-    this.after("each", Orders, (res, req) => calculateButtonAvailability(res, req));
-    this.after("each", Orders.drafts, (res, req) => calculateButtonAvailability(res, req, true));
 
+    this.calculateOrderID = async function (req) {
+      const { maxID } = await SELECT.one`max(orderID) as maxID`.from(Orders);
+      req.data.orderID = maxID + 1;
+    }
+
+    this.calculateItemID = async function (req, itemDraft) {
+      if (!itemDraft.itemID) {
+        const { maxID } = await SELECT.one("max(itemID) as maxID")
+          .from(req.query.target)
+          .where({ order_ID: itemDraft.order_ID });
+        req.data.itemID = maxID + 10;
+      }
+    }
+
+    this.calculateItemAmount = function (req, product, quantity) {
+      req.data.itemAmount = product.price * quantity;
+      req.data.currency_code = product.currency_code;
+    }
+
+    this.checkProductStock = function (req, product, quantity) {
+      if (product.stock < quantity) {
+        req.error(409, `${quantity} exceeds stock for product #${product.productID}`, "quantity");
+      }
+    }
 
     return super.init();
   }
